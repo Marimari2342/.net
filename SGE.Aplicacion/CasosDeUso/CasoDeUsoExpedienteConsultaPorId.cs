f21b2c292540c10d20c48aa39b325def841a4b4e
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-27 20:52:16 -0300

    coomit

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteConsultaPorId.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteConsultaPorId.cs
@@ -2,21 +2,5 @@
 
 public class CasoDeUsoExpedienteConsultaPorId
 {
-    private readonly IExpedienteRepositorio _expedienteRepositorio;
-    private readonly ITramiteRepositorio _tramiteRepositorio;
-
-    public CasoDeUsoExpedienteConsultaPorId(IExpedienteRepositorio expedienteRepositorio, ITramiteRepositorio tramiteRepositorio)
-    {
-        _expedienteRepositorio = expedienteRepositorio;
-        _tramiteRepositorio = tramiteRepositorio;
-    }
-
-    public Expediente ConsultarConTramites(int idExpediente)
-    {
-        // Obtener expediente
-        Expediente expediente = _expedienteRepositorio.ObtenerPorId(idExpediente);
-
-        // Obtener trámites asociados al expediente
-        //IEnumerable;
-    }
+    
 }
--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteConsultaPorEtiqueta.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteConsultaPorEtiqueta.cs
@@ -2,5 +2,21 @@ namespace SGE.Aplicacion;
 
 public class CasoDeUsoTramiteConsultaPorEtiqueta
 {
-    
+    private readonly IExpedienteRepositorio _expedienteRepositorio;
+    private readonly ITramiteRepositorio _tramiteRepositorio;
+
+    public CasoDeUsoTramiteConsultaPorEtiqueta(IExpedienteRepositorio expedienteRepositorio, ITramiteRepositorio tramiteRepositorio)
+    {
+        _expedienteRepositorio = expedienteRepositorio;
+        _tramiteRepositorio = tramiteRepositorio;
+    }
+
+    public Expediente ConsultarConTramites(int idExpediente)
+    {
+        // Obtener expediente
+        Expediente expediente = _expedienteRepositorio.ObtenerPorId(idExpediente);
+
+        // Obtener trámites asociados al expediente
+        //IEnumerable;
+    }
 }
