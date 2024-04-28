c764041eafc77b788b59e8aa43ac5da03c63d6f8
Camila <camilaamilivia13@gmail.com>
2024-04-28 11:20:01 -0300

    cambios a validaciones

--- SGE.Aplicacion/Validadores/ExpendienteValidador.cs
+++ SGE.Aplicacion/Validadores/ExpendienteValidador.cs
@@ -5,9 +5,9 @@ public class ExpendienteValidador
     public bool Validar(Expediente expediente)
     {
         // Validar que la carátula no esté vacía
-        if (string.IsNullOrEmpty(expediente.Caratula))
+        if (expediente.Caratula == null)
             return false;
-
+            
         // Validar que el Id de usuario sea válido
         if (expediente.IdUsuarioUltimaModificacion <= 0)
             return false;
