1b35ddd0878561f28ff1b092441227ae1e52334d
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-28 11:35:43 -0300

    commit expediente baja

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteBaja.cs
@@ -1,11 +1,40 @@
 namespace SGE.Aplicacion;
 
 /*ExpedienteBaja: Puede realizar bajas de expedientes
+Necesito -->    El ID del expediente
+                El ID del usuario que lo quiere dar de baja
+                Verificar que el usuario tenga el permiso para esto
+                Verificar que el ID del expediente sea válido
+                Anotar la fecha de eliminacion (?) creo que no....
+                Eliminar expediente
 Se debe garantizar que para las operaciones de alta, baja y modificación se verifique la autorización del
 usuario antes de proceder. Por lo tanto, el método Ejecutar de estos casos de uso deberá recibir también el
 Id del usuario como parámetro.*/
 
 public class CasoDeUsoExpedienteBaja
 {
+    private readonly IExpedienteRepositorio _expedienteRepositorio;
 
+    public CasoDeUsoExpedienteBaja(IExpedienteRepositorio expedienteRepositorio)
+    {
+        _expedienteRepositorio = expedienteRepositorio;
+    }
+
+    public void Ejecutar(int idExpediente, int idUsuario)
+    {
+        // Verificar que el ID del expediente sea válido
+        if (expedienteRepositorio.ObtenerPorId(idExpediente)==null)
+        {
+            throw new RepositorioException("El expediente buscado no existe")
+        }
+
+        // Verificar permisos
+        if (!_servicioAutorizacion.PoseeElPermiso(idUsuario, Permiso.ExpedienteBaja))
+        {
+            throw new AutorizacionException("El usuario no tiene permiso para realizar esta operación.");
+        }
+
+        // Eliminar expediente en el repositorio
+        _expedienteRepositorio.Eliminar(expediente);
+    }
 }
--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteAlta.cs
@@ -4,5 +4,5 @@ namespace SGE.Aplicacion;
 
 public class CasoDeUsoTramiteAlta
 {
-    
+
 }
