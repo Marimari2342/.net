1a82aab1dd2144a1f06eb60191f6b6ee2540de97
Consuelo Vereau <168292313+ConsueloVT@users.noreply.github.com>
2024-04-30 17:58:05 -0300

    Update TramiteRepositorioTXT.cs

--- SGE.Repositorios/TramiteRepositorioTXT.cs
+++ SGE.Repositorios/TramiteRepositorioTXT.cs
@@ -3,5 +3,19 @@ using SGE.Aplicacion;
 
 public class RepositorioTramiteTXT : ITramiteRepositorio
 {
+    readonly string _nombreArch = "tramites.txt";
+    static int cantT=0;
+
+    public void Agregar(Tramite t){
+        using var sw = new StreamWriter(_nombreArch, true);
+        sw.WriteLine(t.Id=cantT++);  //?
+        sw.WriteLine(t.ExpedienteId);
+        sw.WriteLine(t.Etiqueta);
+        sw.WriteLine(t.Contenido);
+        sw.WriteLine(t.FechaCreacion);
+        sw.WriteLine(t.UltimaModificacion);
+        sw.WriteLine(t.IdUsuarioUltimaModificacion);
+        
+    }
 
 }
