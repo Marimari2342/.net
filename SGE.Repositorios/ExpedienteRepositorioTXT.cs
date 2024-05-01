baeb0e1827f1530475510bc66bc9becb42ff2c89
Consuelo Vereau <168292313+ConsueloVT@users.noreply.github.com>
2024-05-01 17:12:42 -0300

    Update ExpedienteRepositorioTXT.cs

--- SGE.Repositorios/ExpedienteRepositorioTXT.cs
+++ SGE.Repositorios/ExpedienteRepositorioTXT.cs
@@ -4,18 +4,45 @@ using SGE.Aplicacion;
 public class ExpedienteRepositorioTXT : IExpedienteRepositorio
 {
     
-    readonly string _nombreArch = "expedientes.txt";
-    static int cantE=0;
+    readonly string _nombreArch;
+    static int cantE=0; //???
 
     
     public void Agregar(Expediente e){
         using var sw = new StreamWriter(_nombreArch, true);
-        sw.WriteLine(e.Id=cantE++);  // ? 
+        sw.WriteLine(e.Id=cantE++);  // ??? 
         sw.WriteLine(e.Caratula);
         sw.WriteLine(e.FechaCreacion);
         sw.WriteLine(e.UltimaModificacion);
         sw.WriteLine(e.IdUsuarioUltimaModificacion);
         sw.WriteLine(e.Estado);
+    }
+
+    public Expediente ObtenerPorId(int id){
+        Expediente? resultado = null;
+        //Codigo...
+    
+        if( resultado == null ){
+            throw new RepositorioException($"No existe un expediente con id: {id}");
+        }
+        return resultado;   
+    }
+
+    public void Eliminar(int id){
+        bool ok=false;
+        //Codigo
+        if(!ok){
+            throw new RepositorioException($"El expediente con el id={id} no existe");
+        }
+
+    }
+
+    public void Modificar(Expediente e,int id){
+        bool ok=false;
+        //Codigo
+        if(!ok){
+            throw new RepositorioException($"El expediente con el id={id} no existe");
+        }
     }
 
 }
