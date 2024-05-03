a391dd26d6bfe013ee5157dfac43df0a7eab4c6e
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-02 23:16:34 -0300

    commit

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteAlta.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoExpedienteAlta.cs
@@ -4,7 +4,9 @@
 ExpedienteAlta: Puede realizar altas de expedientes
 Se debe garantizar que para las operaciones de alta, baja y modificación se verifique la autorización del
 usuario antes de proceder. Por lo tanto, el método Ejecutar de estos casos de uso deberá recibir también el
-Id del usuario como parámetro.*/
+Id del usuario como parámetro.
+Chequear que no se intenta acceder a un repos que no existe
+En los casos uso try catch o como?*/
 
 public class CasoDeUsoExpedienteAlta
 {
