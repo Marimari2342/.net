9a6ce4dec2b0c4edf19d1012e9018c98379d7b00
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-04 17:03:26 -0300

    commit

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteConsultaPorEtiqueta.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoTramiteConsultaPorEtiqueta.cs
@@ -4,5 +4,5 @@ public class CasoDeUsoTramiteConsultaPorEtiqueta
 {
     /*Lista todos los trámites en el sistema que posean una etiqueta 
     específica (de los enumerativos EtiquetaTramite)... por ejemplo,
-    todos los que tienen la etiqueta = PaseAEstudio  */
+    todos los que tienen la etiqueta = PaseAEstudio*/
 }
