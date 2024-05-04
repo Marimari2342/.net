81fa0bba30dba799595006c6b74be4eeb1907fd8
Laura <leguizamonlauram@gmail.com>
2024-05-04 17:41:11 -0300

    ToString Expediente

--- SGE.Aplicacion/Entidades/Expediente.cs
+++ SGE.Aplicacion/Entidades/Expediente.cs
@@ -2,11 +2,15 @@
 
 public class Expediente: IExpedienteRepositorio, IServicioAutorizacion
 {
-    public int Id { get; set; }
-    //Probando Mariaaannn
-    public string Caratula { get; set; }
-    public DateTime FechaCreacion { get; set; }
-    public DateTime UltimaModificacion { get; set; }
-    public int IdUsuarioUltimaModificacion { get; set; }
-    public EstadoExpediente Estado { get; set; }
+    public int Id { get; set; } //Id del expediente (identificador numérico único)
+    public string Caratula { get; set; } //texto ingresado por el usuario, no puede estar vacío
+    public DateTime FechaCreacion { get; set; } //fecha de creacion del expediente, se indica en la creación del espediente y no se puede cambiar
+    public DateTime UltimaModificacion { get; set; } //fecha de ultima modificación, cada vez que modifico, cambia
+    public int IdUsuarioUltimaModificacion { get; set; } //identificacion del ultimo usuario que modificó el expediente
+    public EstadoExpediente Estado { get; set; } //posibles valores enumerativos = EstadoExpediente
+
+public override string ToString()
+    {
+    return $"Id: {Id}, Caratula: {Caratula}, Fecha de creación: {FechaCreacion}, Última modificación: {UltimaModificacion}, Id del usuario con última modificación: {IdUsuarioUltimaModificacion}, Estado del expediente: {EstadoExpediente}";
+    }
 }
