b09c8510c12836ab32908febeb63170101b92dc8
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-04 19:46:11 -0300

    commit tramite validador mejorado

--- SGE.Aplicacion/Validadores/TramiteValidador.cs
+++ SGE.Aplicacion/Validadores/TramiteValidador.cs
@@ -4,14 +4,8 @@ public class TramiteValidador
 {
      public bool Validar(Tramite tramite)
     {
-        // Validar que el contenido del tramite no esté vacío
-        if (tramite.Contenido == null)
-            return false;
-
-        // Validar que el Id de usuario del tramite sea válido
-        if (tramite.IdUsuarioUltimaModificacion <= 0)
-            return false;
-
-        return true;
+        /* Validar que el contenido del tramite no esté vacío
+         Validar que el Id de usuario del tramite sea válido*/
+        return (tramite.Contenido != null) && (tramite.IdUsuarioUltimaModificacion > 0);
     }
 }
