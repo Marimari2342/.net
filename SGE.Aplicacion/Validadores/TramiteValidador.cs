03c46592616d40e02aa15e1354a91b97de5c31de
Consuelo Vereau <168292313+ConsueloVT@users.noreply.github.com>
2024-05-12 14:32:02 -0300

    Update TramiteValidador.cs

--- SGE.Aplicacion/Validadores/TramiteValidador.cs
+++ SGE.Aplicacion/Validadores/TramiteValidador.cs
@@ -1,8 +1,8 @@
 ﻿namespace SGE.Aplicacion;
 
-public class TramiteValidador
+public class static TramiteValidador
 {
-     public bool Validar(Tramite tramite)
+     public static bool Validar(Tramite tramite)
     {
         /* Validar que el contenido del tramite no esté vacío
          Validar que el Id de usuario del tramite sea válido*/
